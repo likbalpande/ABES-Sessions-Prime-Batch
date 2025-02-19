96dc0ca501db064e1dddfd680cc678a04f67e83b
Likhilesh <likbalpande@example.com>
2025-02-19 15:22:57 +0530

    dom basics

---
+++ day5-js/domBasics.js
@@ -0,0 +1,20 @@
+// console.dir(window); --> window object represents : B.O.M
+// console.dir(document); --> document object represents : D.O.M
+// document is given by window. So, window.document --> document
+
+// write generic text in html
+// document.write("Hello");
+
+// finding / searching the element
+// 1. document.getElementById("id-name") --> null / element
+// 2. document.getElementsByClassName("class-name") --> iterable [] / [element, element, ....]
+// 3. document.getElementsByTagName() --> iterable [] / [element, element, ....]
+// 4. document.querySelector() -->
+// 5. document.querySelectorAll() -->
+
+// 1st method
+// const elem = document.getElementById("text-1");
+// console.dir(elem);
+// elem.style.color = "blue";
+
+// ----------------------------------------------------------
