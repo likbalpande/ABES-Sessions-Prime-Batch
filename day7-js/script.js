7fd638bbee67f8d43e84d7799ebd00b3bc0bdd00
Likhilesh <likbalpande@example.com>
2025-02-21 16:43:22 +0530

    JS - CRUD

--- day7-js/script.js
+++ day7-js/script.js
@@ -1,27 +1,39 @@
 const data = [
     {
-        id: "abcd1",
+        email: "rakesh@gmail.com",
         name: "Rakesh",
         city: "Delhi",
     },
     {
-        id: "a1",
+        email: "mohan@gmail.com",
         name: "Mohan",
         city: "Noida",
     },
     {
-        id: "b2",
+        email: "rishabh@gmail.com",
         name: "Rishabh",
         city: "Mumbai",
     },
     {
-        id: "abcd3",
+        email: "ramesh@gmail.com",
         name: "Ramesh",
         city: "Delhi",
     },
 ];
 
 const root = document.getElementById("root");
+const selectElement = document.getElementsByTagName("select")[0];
+
+const cities = data.map((elem) => elem.city);
+
+const showOptions = () => {
+    cities.forEach((city) => {
+        const newOption = document.createElement("option");
+        newOption.value = city;
+        newOption.innerText = city;
+        selectElement.appendChild(newOption);
+    });
+};
 
 const showCards = (newData) => {
     root.innerHTML = "";
@@ -31,13 +43,13 @@ const showCards = (newData) => {
         card.innerHTML = `
             <h4>${elem.name}</h4>
             <p>${elem.city}</p>
-            <button onClick="deleteCard(event, '${elem.id}')">Delete</button>
+            <button onClick="deleteCard(event, '${elem.email}')">Delete</button>
         `;
         root.appendChild(card);
     });
 };
 
-const deleteCard = (e, elemId) => {
+const deleteCard = (e, elemEmail) => {
     // console.log(e.target.parentElement);
     // e.target.remove();
     // -- one way to delete
@@ -47,7 +59,7 @@ const deleteCard = (e, elemId) => {
     // data.splice(idx, 1);
     // showCards(data);
     // -- correct way
-    const index = data.findIndex((elem) => elem.id == elemId);
+    const index = data.findIndex((elem) => elem.email == elemEmail);
     data.splice(index, 1);
     showCards(data);
 };
@@ -59,6 +71,38 @@ const handleSelect = (e) => {
         return false;
     });
     showCards(newData);
+};
+
+const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // console.log(e);
+    // console.log(e.target.email.value);
+    // console.log(e.target[0].value);
+    // console.log(e.target.fullName.value);
+    // console.log(e.target[1].value);
+    // console.log(e.target.city.value);
+    // console.log(e.target[2].value);
+
+    // add the data
+
+    const isEmailExists = data.some((elem) => {
+        return elem.email === e.target.email.value;
+    });
+
+    if (isEmailExists) {
+        alert("Email already exists");
+
+        return;
+    }
+
+    const newElem = {
+        name: e.target.fullName.value,
+        email: e.target.email.value,
+        city: e.target.city.value,
+    };
+
+    data.push(newElem);
+    showCards(data);
 };
 
 showCards(data);
