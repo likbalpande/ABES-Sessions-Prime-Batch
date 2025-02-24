7dcae091aba2f8a17881232171862434e6d5af3b
Likhilesh <likbalpande@example.com>
2025-02-24 17:02:45 +0530

    localstorage for retrieval of old data

--- day8-js/async_callbacks.js
+++ day8-js/async_callbacks.js
@@ -242,10 +242,33 @@ function showUI(data) {
     // responsible for creating a visualization
     console.log("data:", data);
     const { avatar_url, login, html_url } = data;
-    const card = document.getElementById("card");
-    card.innerHTML = `
+    const root = document.getElementById("cards");
+    const newCard = document.createElement("div");
+    newCard.id = "card";
+    newCard.innerHTML = `
         <h4>${login.toUpperCase()}</h4>
         <img src='${avatar_url}' alt='avatar'>
         <a href=${html_url}>View Profile</a>
     `;
+    root.appendChild(newCard);
+
+    storeData(data);
 }
+
+function storeData(data) {
+    const oldData = localStorage.getItem("searches"); // returns a JSON string
+    const arr = JSON.parse(oldData || "[]");
+    arr.push(data);
+    localStorage.setItem("searches", JSON.stringify(arr)); //key, value
+}
+
+function showHistory() {
+    const oldData = localStorage.getItem("searches"); // returns a JSON string
+    const arr = JSON.parse(oldData || "[]");
+    // if(oldData === undefined) arr = JSON.stringify('[]');
+    arr.forEach((data) => {
+        showUI(data);
+    });
+}
+
+showHistory();
