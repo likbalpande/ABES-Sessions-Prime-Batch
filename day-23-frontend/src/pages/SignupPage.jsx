ecde30709127477280766e01dbc004ac077eb5b2
Likhilesh <likbalpande@example.com>
2025-05-02 17:00:20 +0530

    signup page - part 1

--- day-23-frontend/src/pages/SignupPage.jsx
+++ day-23-frontend/src/pages/SignupPage.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 export const SignupPage = () => {
+    const [sendingOtp, setSendingOtp] = useState(false);
+    const [isOtpSent, setIsOtpSent] = useState(false);
+    const [email, setEmail] = useState("");
+
     const handleSendOtp = async (e) => {
         try {
+            setSendingOtp(true);
             e.preventDefault();
 
             const email = e.target.email.value;
+            setEmail(email);
 
             const resp = await fetch("http://localhost:2200/api/v1/auth/otps", {
                 method: "POST",
@@ -16,10 +24,23 @@ export const SignupPage = () => {
             const data = await resp.json();
 
             console.log(data);
+            setIsOtpSent(true);
         } catch (err) {
             console.log("Error sending otp", err.message);
+        } finally {
+            setSendingOtp(false);
         }
     };
+
+    if (isOtpSent) {
+        return (
+            <div>
+                <form>
+                    <input value={email} disabled />
+                </form>
+            </div>
+        );
+    }
 
     return (
         <div>
@@ -28,7 +49,9 @@ export const SignupPage = () => {
                     <input type="email" name="email" require />
                 </div>
                 <div>
-                    <button>Send OTP</button>
+                    <button disabled={sendingOtp} className="">
+                        {sendingOtp ? "Sending..." : "Send OTP"}
+                    </button>
                 </div>
             </form>
         </div>
